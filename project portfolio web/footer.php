bf45ea465272c54ca0312728fdbbe0300d6de2e4
Rahmat Adi Nugroho <89901742+rahmatadinugroho@users.noreply.github.com>
2022-10-08 11:37:02 +0700

    Add files via upload

---
+++ project portfolio web/footer.php
@@ -0,0 +1,24 @@
+    <footer>
+        <div class="container">
+            <div class="row">
+                <!-- contact -->
+                <div class="col-sm-6 mt-4 mb-4">
+                    <h3>Contact</h3>
+                    <ul>
+                        <li>rahmatadinugroho.m1@gmail.com</li>
+                        <li>0878-8987-8893</li>
+                    </ul>
+                </div>
+
+                <!-- social media -->
+                <div class="col-sm-6 mt-4 mb-4">
+                    <h3>Social Media</h3>
+                    <ul>
+                        <li>@r_adi_n</li>
+                        <li>@terrabite39</li>
+                        <li>TerraBite</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
